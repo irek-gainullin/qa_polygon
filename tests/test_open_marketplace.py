e5b34153fd4eda3ca1e646eb95f70a2c6b44cf07
irek <ire.a.gainullin@gmail.com>
2026-03-05 13:11:55 +0300

    annotation update #2

--- tests/test_open_marketplace.py
+++ tests/test_open_marketplace.py
@@ -14,14 +14,14 @@ class TestOpenMarketplace:
         # 1. Переход на страницу моделей
         models_page.visit(AppRoute.MODELS)
 
-        # 2. Проверка, что форма регистрации отображается
+        # 2. Проверка, что логотип SuperProtocol отображается
         models_page.header_navbar.logo.check_visible()
 
         # 3. Проверка что отображается кнопка Enter Marketplace
         #models_page.header_navbar.enter_marketplace_button.check_visible()
 
-        #4. Клик по кнопке регистрации
+        #4. Клик по кнопке Enter Marketplace
         #models_page.header_navbar.enter_marketplace_button.click()
 
-        # 5. Проверка отображения элементов на Dashboard
+        # 5. Проверка отображения элементов на Pop-up
         #models_page.enter_marketplace_popup.text.check_visible()
